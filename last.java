486fcad684a78b02a6c081dd3bd9d0a29b69385e
danielvjacob <98068637+danielvjacob@users.noreply.github.com>
2023-02-15 22:31:43 -0600

    added main function

--- last.java
+++ last.java
@@ -1,6 +1,23 @@
 package cs_3203_last;
-
+import java.util.*;
 public class last {
+    public static void main(String[] args) throws Exception{
+        int[] array = new int[5];
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i++){
+            int number;
+            System.out.println("Enter number: ");
+            number = scan.nextInt();
+            array[i] = number;
+        }
+        
+        int sum = Sum(array);
+        int product = Product(array);
+
+        System.out.println("The sum of the array is: " + sum);
+        System.out.println("The product of the array is: " + product);
+    }
     public static int Sum(int[] array) {
         int result = 0;
         for (int i = 0; i < array.length; i++){
